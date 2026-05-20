c71f6d6cdf460f702013878732e3ca3dc1b7828f
Paris Kolovos <pariskolovos@live.com>
2026-05-20 21:08:21 +0300

    simplify properties loader

--- src/main/java/gr/kgdev/sqlemur/utils/SQLemurProperties.java
+++ src/main/java/gr/kgdev/sqlemur/utils/SQLemurProperties.java
@@ -2,8 +2,6 @@ package gr.kgdev.sqlemur.utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Properties;
@@ -16,43 +14,15 @@ public class SQLemurProperties {
 		loadProperties("./sqlemur.properties");
 	}
 	
-	public static void loadProperties(String rootPath) {
-		try {
-			Files.walk(Paths.get(rootPath))
-	        .filter(Files::isRegularFile)
-	        .filter(path -> path.toString().endsWith(".properties"))
-	        .forEach(path -> {
-	        	try (InputStream inputStream = new FileInputStream(path.toFile())) {
-					Properties props = new Properties();
-					props.load(inputStream);
-					propertiesMap.put(path.getFileName().toString(), props);
-		        } catch (IOException e) {
-	        		e.printStackTrace();
-				}
-	        });
-		} catch (IOException e) {
-    		e.printStackTrace();
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T>T getProperty(String fileKey, String key, Class<?> clazz) {
-		var value = propertiesMap.get(fileKey).get(key);
-		try {
-			var cons = clazz.getConstructor(String.class);
-			var returnedValue = cons.newInstance(value.toString());
-			return (T) returnedValue;
-		} catch (Exception e) {
-    		e.printStackTrace();
-		}
-		return null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T>T getProperty(String fileKey, String key, Class<?> clazz, Object defaultValue) {
-		var value = getProperty(fileKey, key, clazz);
-		return value != null ? (T) value : (T) defaultValue;
-	}
+    public static void loadProperties(String filePath) {
+        try (var inputStream = new FileInputStream(filePath)) {
+            Properties props = new Properties();
+            props.load(inputStream);
+            propertiesMap.put(Paths.get(filePath).getFileName().toString(), props);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	/**
 	 * Returns the first matching key from all loaded properties
@@ -91,14 +61,5 @@ public class SQLemurProperties {
 		var value = getProperty(key, clazz);
 		return value != null ? (T) value : (T) defaultValue;
 	}
-	
-	public static Properties getPropertiesFromFile(String fileNamePart) {
-		for (var key : propertiesMap.keySet()) {
-			if (key.contains(fileNamePart))
-				return propertiesMap.get(key);
-		}
-		return null;
-	}
-	
 	
 }
