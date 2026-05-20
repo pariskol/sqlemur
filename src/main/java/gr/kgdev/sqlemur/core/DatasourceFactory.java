d32bb87dd15fba9e7b2f0aa820bab6ba61b6e4d9
Paris Kolovos <pariskolovos@live.com>
2026-05-20 21:09:28 +0300

    reduce default connection pool size to be lighter

--- src/main/java/gr/kgdev/sqlemur/core/DatasourceFactory.java
+++ src/main/java/gr/kgdev/sqlemur/core/DatasourceFactory.java
@@ -16,9 +16,9 @@ public class DatasourceFactory {
 		dbcp2DataSource.setUrl(url);
 		dbcp2DataSource.setUsername(user);
 		dbcp2DataSource.setPassword(password);
-		dbcp2DataSource.setInitialSize((Integer) SQLemurProperties.getProperty("sqlemur.pool.initialsize", Integer.class, 4));
-		dbcp2DataSource.setMaxIdle((Integer) SQLemurProperties.getProperty("sqlemur.pool.idlesize", Integer.class, 16));
-		dbcp2DataSource.setMaxTotal((Integer) SQLemurProperties.getProperty("sqlemur.pool.maxsize", Integer.class, 32));
+		dbcp2DataSource.setInitialSize((Integer) SQLemurProperties.getProperty("sqlemur.pool.initialsize", Integer.class, 2));
+		dbcp2DataSource.setMaxIdle((Integer) SQLemurProperties.getProperty("sqlemur.pool.idlesize", Integer.class, 4));
+		dbcp2DataSource.setMaxTotal((Integer) SQLemurProperties.getProperty("sqlemur.pool.maxsize", Integer.class, 8));
 		var maxWaitMillis = (Integer) SQLemurProperties.getProperty("sqlemur.pool.maxwaitmillis", Integer.class, 10000);
 		dbcp2DataSource.setMaxWait(Duration.ofMillis(maxWaitMillis));
 		return dbcp2DataSource;
